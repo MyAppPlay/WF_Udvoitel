df5d18d075b44418cef3ad35a9922ce42371c8a3
MyAppPlay <Tehmaser@gmail.com>
2019-06-20 11:16:39 +0300

    ДЗ 1 Готово

--- WF_Udvoitel/Form1.Designer.cs
+++ WF_Udvoitel/Form1.Designer.cs
@@ -41,84 +41,126 @@
             // 
             // btnCommand1
             // 
-            this.btnCommand1.Location = new System.Drawing.Point(246, 254);
+            this.btnCommand1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnCommand1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCommand1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCommand1.Location = new System.Drawing.Point(580, 202);
+            this.btnCommand1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnCommand1.Name = "btnCommand1";
-            this.btnCommand1.Size = new System.Drawing.Size(113, 58);
+            this.btnCommand1.Size = new System.Drawing.Size(188, 89);
             this.btnCommand1.TabIndex = 0;
             this.btnCommand1.Text = "+1";
-            this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.UseVisualStyleBackColor = false;
             this.btnCommand1.Click += new System.EventHandler(this.BtnCommand1_Click);
             // 
             // btnCommand2
             // 
-            this.btnCommand2.Location = new System.Drawing.Point(246, 318);
+            this.btnCommand2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnCommand2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCommand2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCommand2.Location = new System.Drawing.Point(580, 398);
+            this.btnCommand2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnCommand2.Name = "btnCommand2";
-            this.btnCommand2.Size = new System.Drawing.Size(113, 58);
+            this.btnCommand2.Size = new System.Drawing.Size(188, 89);
             this.btnCommand2.TabIndex = 1;
             this.btnCommand2.Text = "x2";
-            this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.UseVisualStyleBackColor = false;
             this.btnCommand2.Click += new System.EventHandler(this.BtnCommand2_Click);
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(246, 382);
+            this.btnReset.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnReset.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnReset.Location = new System.Drawing.Point(305, 580);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(113, 59);
+            this.btnReset.Size = new System.Drawing.Size(200, 91);
             this.btnReset.TabIndex = 2;
             this.btnReset.Text = "Сброс";
-            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.UseVisualStyleBackColor = false;
             this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
             // 
             // lblNumber
             // 
-            this.lblNumber.AutoSize = true;
-            this.lblNumber.Location = new System.Drawing.Point(283, 134);
+            this.lblNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNumber.BackColor = System.Drawing.Color.PaleGreen;
+            this.lblNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNumber.Cursor = System.Windows.Forms.Cursors.No;
+            this.lblNumber.Font = new System.Drawing.Font("Yu Gothic Medium", 75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblNumber.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblNumber.Location = new System.Drawing.Point(305, 55);
+            this.lblNumber.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.lblNumber.MaximumSize = new System.Drawing.Size(200, 200);
+            this.lblNumber.MinimumSize = new System.Drawing.Size(167, 154);
             this.lblNumber.Name = "lblNumber";
-            this.lblNumber.Size = new System.Drawing.Size(39, 13);
+            this.lblNumber.Size = new System.Drawing.Size(200, 200);
             this.lblNumber.TabIndex = 3;
             this.lblNumber.Text = "Число";
+            this.lblNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblChekC2
             // 
-            this.lblChekC2.AutoSize = true;
-            this.lblChekC2.Location = new System.Drawing.Point(12, 134);
+            this.lblChekC2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblChekC2.Cursor = System.Windows.Forms.Cursors.No;
+            this.lblChekC2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblChekC2.ForeColor = System.Drawing.Color.Navy;
+            this.lblChekC2.Location = new System.Drawing.Point(40, 398);
+            this.lblChekC2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblChekC2.Name = "lblChekC2";
-            this.lblChekC2.Size = new System.Drawing.Size(117, 13);
+            this.lblChekC2.Size = new System.Drawing.Size(188, 91);
             this.lblChekC2.TabIndex = 4;
             this.lblChekC2.Text = "Количество операций";
+            this.lblChekC2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.BackgroundImage = global::WF_Udvoitel.Properties.Resources.ruin_wall_03_height;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.PlayMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(602, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(10, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(807, 64);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // PlayMenuItem
             // 
+            this.PlayMenuItem.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold);
+            this.PlayMenuItem.ForeColor = System.Drawing.Color.Red;
             this.PlayMenuItem.Name = "PlayMenuItem";
-            this.PlayMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.PlayMenuItem.Size = new System.Drawing.Size(170, 58);
             this.PlayMenuItem.Text = "Играть";
+            this.PlayMenuItem.TextAlign = System.Drawing.ContentAlignment.BottomRight;
             this.PlayMenuItem.Click += new System.EventHandler(this.PlayMenuItem_Click);
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(15, 53);
+            this.btnBack.BackColor = System.Drawing.Color.Bisque;
+            this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBack.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnBack.Location = new System.Drawing.Point(46, 202);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(114, 48);
+            this.btnBack.Size = new System.Drawing.Size(182, 89);
             this.btnBack.TabIndex = 6;
             this.btnBack.Text = "Вернуть ход";
-            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.BtnBack_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(602, 486);
+            this.BackgroundImage = global::WF_Udvoitel.Properties.Resources.ruin_wall_03_height;
+            this.ClientSize = new System.Drawing.Size(807, 685);
+            this.ControlBox = false;
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.lblChekC2);
             this.Controls.Add(this.lblNumber);
@@ -126,8 +168,15 @@
             this.Controls.Add(this.btnCommand2);
             this.Controls.Add(this.btnCommand1);
             this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(823, 748);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(823, 690);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Удвоитель";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -141,11 +190,11 @@
         private System.Windows.Forms.Button btnCommand1;
         private System.Windows.Forms.Button btnCommand2;
         private System.Windows.Forms.Button btnReset;
-        private System.Windows.Forms.Label lblNumber;
         private System.Windows.Forms.Label lblChekC2;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem PlayMenuItem;
         private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblNumber;
     }
 }
 
