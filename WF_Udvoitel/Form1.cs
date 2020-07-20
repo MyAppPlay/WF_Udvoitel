108614f1a30ca100532b7e1a4f636d9fb92e86d6
Владимир Гаврилов <242440@list.ru>
2020-07-20 18:53:56 +0300

    Готово

--- WF_Udvoitel/Form1.cs
+++ WF_Udvoitel/Form1.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 //      Задание 1
-//а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
-//б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число должен получить игрок.
+// а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
+// б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число должен получить игрок.
 //    Игрок должен получить это число за минимальное количество ходов.
-//в) * Добавить кнопку «Отменить», которая отменяет последние ходы.Используйте обобщенный класс Stack.
-// Вся логика игры должна быть реализована в классе с удвоителем.
+// в) * Добавить кнопку «Отменить», которая отменяет последние ходы.Используйте обобщенный класс Stack.
+//      Вся логика игры должна быть реализована в классе с удвоителем.
 //                 Гаврилов Владимир
 
 namespace WF_Udvoitel
@@ -23,27 +17,34 @@ namespace WF_Udvoitel
         public Form1()
         {
             InitializeComponent();
-            btnCommand1.Text = "+1";
-            btnCommand2.Text = "x2";
-            btnReset.Text = "Сброс";
-            lblNumber.Text = "1";
-            this.Text = "Удвоитель";
-            lblChekC2.Text = "0";//переменная подсчета отдельных команд
+            btnCommand1.Text = @"+1";
+            btnCommand2.Text = @"x2";
+            btnReset.Text = @"Сброс";
+            lblNumber.Text = @"1";
+            Text = @"Удвоитель";
+            lblChekC2.Text = @"0";//переменная подсчета отдельных команд
 
 
 
         }
-        public Stack<string> revers = new Stack<string>();//Массив стека
+
+        public sealed override string Text
+        {
+            get => base.Text;
+            set => base.Text = value;
+        }
+
+        public Stack<string> Revers = new Stack<string>();//Массив стека
 
         private void BtnCommand1_Click(object sender, EventArgs e)
         {
             lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
             lblChekC2.Text = (int.Parse(lblChekC2.Text) + 1).ToString();
-            revers.Push(lblNumber.Text);
-            if (i == int.Parse(lblChekC2.Text) && proverka == int.Parse(lblNumber.Text))
+            Revers.Push(lblNumber.Text);
+            if (_i == int.Parse(lblChekC2.Text) && _proverka == int.Parse(lblNumber.Text))
             {
-                MessageBox.Show("Вам это удалось", "Победа");
-                i = 0;
+                MessageBox.Show(@"Вам это удалось", @"Победа");
+                _i = 0;
             }
 
 
@@ -53,19 +54,19 @@ namespace WF_Udvoitel
         {
             lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
             lblChekC2.Text = (int.Parse(lblChekC2.Text) + 1).ToString();
-            revers.Push(lblNumber.Text);
-            if (i == int.Parse(lblChekC2.Text) && proverka == int.Parse(lblNumber.Text))
+            Revers.Push(lblNumber.Text);
+            if (_i == int.Parse(lblChekC2.Text) && _proverka == int.Parse(lblNumber.Text))
             {
-                MessageBox.Show("Вам это удалось", "Победа");
-                i = 0;
+                MessageBox.Show(@"Вам это удалось", @"Победа");
+                _i = 0;
             }
         }
 
         public void BtnReset_Click(object sender, EventArgs e)
         {
-            lblNumber.Text = "1";
-            lblChekC2.Text = "0";
-            revers.Clear();
+            lblNumber.Text = @"1";
+            lblChekC2.Text = @"0";
+            Revers.Clear();
 
         }
 
@@ -73,32 +74,32 @@ namespace WF_Udvoitel
         {
 
         }
-        int i = 0;//переменная для цикла подсчта минимума ходов
-        int proverka;
+        int _i;//переменная для цикла подсчта минимума ходов
+        int _proverka;
 
         private void PlayMenuItem_Click(object sender, EventArgs e)//кнопка играть
         {
-            i = 0;
+            _i = 0;
             BtnReset_Click(sender, e);
             Random rnd = new Random();
             int chislo = rnd.Next(2, 99);
             int xod = chislo;
-            proverka = chislo;
+            _proverka = chislo;
             while (xod != 1)
             {
 
                 if (xod % 2 == 0)
                 {
-                    i++;
+                    _i++;
                     xod /= 2;
                 }
                 else
                 {
-                    i++;
+                    _i++;
                     xod -= 1;
                 }
             }
-            MessageBox.Show($"Создай число {chislo} за {i} ходов", "Задача");
+            MessageBox.Show($@"Создай число {chislo} за {_i} ходов", @"Задача");
         }
 
 
@@ -106,12 +107,17 @@ namespace WF_Udvoitel
         {
             try
             {
-                revers.Pop();
-                lblNumber.Text = revers.Peek();//присвоение предыдущего значения
+                Revers.Pop();
+                lblNumber.Text = Revers.Peek();//присвоение предыдущего значения
 
                 lblChekC2.Text = (int.Parse(lblChekC2.Text) - 1).ToString();
             }
-            catch { };//устранение реакции на ошибку
+            catch
+            {
+                // ignored
+            }
+
+            //устранение реакции на ошибку
         }
     }
 }
